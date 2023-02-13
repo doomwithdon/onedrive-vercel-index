37187e24ada4c05c1b0e8b029b8b029ffd0933ea
doomwithdon <doomwithdon@yahoo.com>
2023-02-12 22:04:17 -0500

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -24,7 +24,7 @@ module.exports = {
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
-  maxItems: 100,
+  maxItems: 200,
 
   // [OPTIONAL] We use Google Fonts natively for font customisations.
   // You can check and generate the required links and names at https://fonts.google.com.
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Media'],
+  protectedRoutes: [''],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:doomwithdon@xkhk3.onmicrosoft.com',
