681d13129513eda566b837bb92c302192fb9844d
doomwithdon <doomwithdon@yahoo.com>
2023-03-26 01:35:30 -0400

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,11 +38,11 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/spencerwooo/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by Aldon.',
+    'Made with ❤ by Aldon.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ["/",],
+ // protectedRoutes: ["/",],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:doomwithdon@xkhk3.onmicrosoft.com',
@@ -53,7 +53,7 @@ module.exports = {
   links: [
     {
       name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      link: 'https://github.com/doomwithdon/onedrive-vercel-index',
     },
   ],
 
